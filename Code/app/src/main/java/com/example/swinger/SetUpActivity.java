6bfcb405d0558dcb30a8147ecab08f28ae55d907
Spencer Kinsey-Korzym <kinseykorzysh@vcu.edu>
2021-03-04 13:00:40 -0500

    Fixed java bug for setup page

--- Code/app/src/main/java/com/example/swinger/SetUpActivity.java
+++ Code/app/src/main/java/com/example/swinger/SetUpActivity.java
@@ -13,18 +13,13 @@ public class SetUpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_up);
     }
-<<<<<<< HEAD
+
     public void gotoHole1(View view){
         Intent intent = new Intent(this, Hole1Activity.class);
         startActivity(intent);
     }
-    public void goHome(View view){
+    public void goHome(View view) {
         Intent intent = new Intent(this, HomeActivity.class);
         startActivity(intent);
-=======
-    public void openHole1(View view){
-        Intent intent1 = new Intent(this, Hole1Activity.class);
-        startActivity(intent1);
->>>>>>> 0c3766b468efba1da57dda334ec93d41afaf4bed
     }
 }
