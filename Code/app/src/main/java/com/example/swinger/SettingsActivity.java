cce4f780721b0491f2b69d7f66784d56e38914c9
Spencer Kinsey-Korzym <kinseykorzysh@vcu.edu>
2021-03-04 17:22:08 -0500

    Added starting code to remove players from edit player page

--- Code/app/src/main/java/com/example/swinger/SettingsActivity.java
+++ Code/app/src/main/java/com/example/swinger/SettingsActivity.java
@@ -2,7 +2,9 @@ package com.example.swinger;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -11,4 +13,13 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
     }
+
+    public void gotoEditPlayer(View view){
+        Intent intent = new Intent(this, EditPlayersActivity.class);
+        startActivity(intent);
+    }
+
+    public void save(View view){
+    finish();}
 }
+
