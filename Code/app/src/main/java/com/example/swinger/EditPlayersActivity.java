4b0764674e18f590105bca106d3f7ba7301ccb40
Jalen Holmes <jaymoney53198@gmail.com>
2021-04-29 23:25:46 -0400

    added remove functionality for the rest of the players

--- Code/app/src/main/java/com/example/swinger/EditPlayersActivity.java
+++ Code/app/src/main/java/com/example/swinger/EditPlayersActivity.java
@@ -12,10 +12,12 @@ public class EditPlayersActivity extends AppCompatActivity {
     private Button removePlayer2;
     private Button removePlayer3;
     private Button removePlayer4;
+    private TextView addPlayer;
     private TextView player1;
     private TextView player2;
     private TextView player3;
     private TextView player4;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +26,7 @@ public class EditPlayersActivity extends AppCompatActivity {
         removePlayer2 = findViewById(R.id.editPlayer_delete_player2_btn);
         removePlayer3 = findViewById(R.id.editPlayer_delete_player3_btn);
         removePlayer4 = findViewById(R.id.editPlayer_delete_player4_btn);
+        addPlayer = findViewById(R.id.addPlayerBtn);
         player1 = findViewById(R.id.editPlayers_player1);
         player2 = findViewById(R.id.editPlayers_player2);
         player3 = findViewById(R.id.editPlayers_player3);
@@ -33,6 +36,18 @@ public class EditPlayersActivity extends AppCompatActivity {
                     player1.setVisibility(View.GONE);
                 }
                 );
+        removePlayer2.setOnClickListener(view ->{
+            removePlayer2.setVisibility(View.GONE);
+            player2.setVisibility(View.GONE);
+        });
+        removePlayer3.setOnClickListener(view ->{
+            removePlayer3.setVisibility(View.GONE);
+            player3.setVisibility(View.GONE);
+        });
+        removePlayer4.setOnClickListener(view ->{
+            removePlayer4.setVisibility(View.GONE);
+            player4.setVisibility(View.GONE);
+        });
     }
 
 
